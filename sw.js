501c9cb24cd8b2863ba290042d7f3f8175445c63
nickjdenton-dev <nickjdenton@gmail.com>
2026-08-07 09:44:37 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="icp-reporter-v4.0";
+const CACHE="icp-reporter-v4.1";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",e=>{
