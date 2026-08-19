25e67cc23b6ae8199e67f61e511d735d5c7bb6bb
nickjdenton-dev <nickjdenton@gmail.com>
2026-08-19 12:57:14 -0400

    Update service worker cache version to v5.16.11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="icp-reporter-v5.16.10";
+const CACHE="icp-reporter-v5.16.11";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",e=>{
