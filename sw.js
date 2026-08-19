8058207507c520dc96fa2025168610a40ad4796c
nickjdenton-dev <nickjdenton@gmail.com>
2026-08-19 15:01:30 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="icp-reporter-v5.16.11";
+const CACHE="icp-reporter-v5.17.0";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",e=>{
