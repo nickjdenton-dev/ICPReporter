8151bd64b4a32fb1cf74d0aed0e7b5afcd113816
nickjdenton-dev <nickjdenton@gmail.com>
2026-08-19 12:19:29 -0400

    Update cache version to v5.16.10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="icp-reporter-v5.16.9";
+const CACHE="icp-reporter-v5.16.10";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",e=>{
